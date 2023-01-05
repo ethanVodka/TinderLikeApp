663028fc0237e83c08bf0f3c66305e31be120bff
ethan <gohho1008@gmail.com>
2023-01-05 11:46:06 +0900

    フッターまで作成

---
+++ TinderApp/TinderApp/TinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderAppApp.swift
+//  TinderApp
+//
+//  Created by Furkat Ethan on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct TinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
